820e72161e4686c17e88a80ece3ff785cf3c8b3d
Jazmine <84024@alumnos.utleon.edu.mx>
2026-09-08 18:56:20 -0600

    Importar y exportar 08/Sep/2026

--- src/temasTS/03-funciones.ts
+++ src/temasTS/03-funciones.ts
@@ -1,29 +1,50 @@
-
-function sumar(a:number, b:number):number {
-    return a+b
-}
-const resultado=sumar(4,3)
-console.log(resultado)
-
-function multiplicar(n1:number,base:number=6):number{
-    return n1*base;
-}
-console.log(multiplicar(3,5))
-console.log(multiplicar(3))
-
-//const addNumberArrow=(a:number,b:number):string=>{return `${a+b}`}
-
-interface Mascotas{
-    nombre:string,
-    edad:number,
-    raza:string,
-    sexo?:string,
-    vacunado:boolean,
-    mostrarEdad()=>void
+function sumar(a: number, b: number): number {
+    return a + b;
 }
 
-function mostrarMascota(mascota:Mascotas){
-    mascota.edad+=;
+const resultado = sumar(4, 3);
+console.log(resultado);
+
+
+function multiplicar(n1: number, base: number = 6): number {
+    return n1 * base;
+}
+
+console.log(multiplicar(3, 5));
+console.log(multiplicar(3));
+
+
+
+
+interface Mascotas {
+    nombre: string;
+    edad: number;
+    raza: string;
+    sexo?: string;
+    vacunado: boolean;
+    mostrarEdad: () => void;
+}
+
+
+function mostrarMascota(mascota: Mascotas, aumentarEdad: number) {
+    mascota.edad += aumentarEdad;
+
     console.log(mascota);
-    console.log(mascota.mostrarEdad())
+
+    mascota.mostrarEdad();
 }
+
+
+const nuevaMascota: Mascotas = {
+    nombre: "firulais",
+    edad: 5,
+    raza: "pastor aleman",
+    vacunado: true,
+
+    mostrarEdad() {
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+    }
+};
+
+
+mostrarMascota(nuevaMascota, 2);
